da9ec40d920a5f86a6a624a41d51b199242181c5
qwaezrx <hongjw0309@gmail.com>
2023-12-16 07:31:28 +0900

    available to downlaod lora from google drive

--- lora.py
+++ lora.py
@@ -3,6 +3,7 @@ import sys
 from pathlib import Path
 import shutil
 import torch
+import uuid
 import comfy.sd
 import comfy.utils
 import folder_paths
@@ -126,6 +127,7 @@ class XLDB_LoRA:
 class S3Bucket_Load_LoRA:
     def __init__(self):
         self.loaded_lora = None
+        self.lora_name_map = {}
 
     @classmethod
     def INPUT_TYPES(s):
@@ -177,6 +179,14 @@ class S3Bucket_Load_LoRA:
     RETURN_TYPES = ("MODEL", "CLIP")
     FUNCTION = "load_lora"
 
+    def get_local_lora_name(self, lora_name: str):
+        if lora_name in self.lora_name_map:
+            return self.lora_name_map[lora_name]
+        if lora_name.endswith(".safetensors") or "checkpoint" in lora_name:
+            self.lora_name_map[lora_name] = lora_name
+        else:
+            self.lora_name_map[lora_name] = f"{uuid.uuid4()}.safetensors"
+        return self.lora_name_map[lora_name]
 
     def load_lora(self, model, clip, lora_name, strength_model, strength_clip, **bucket_creds):
         """
@@ -196,43 +206,50 @@ class S3Bucket_Load_LoRA:
             if value:
                 os.environ[key] = value
 
-        if lora_name == "None":
+        if lora_name == "None" or not lora_name:
             return model, clip
-        lora_path = folder_paths.get_full_path("loras", lora_name)
+
+        local_lora_path = folder_paths.get_full_path("loras", lora_name)
         lora = None
-        if not lora_path:
-            new_lora_path = Path("/tmp") / "loras"
-            if os.path.exists(new_lora_path):
-                shutil.rmtree(new_lora_path, ignore_errors=False, onerror=None)
+        if not local_lora_path:
+            new_lora_dir = Path("/tmp") / "loras"
+            lora_url_or_path = lora_name
             
-            os.makedirs(new_lora_path, exist_ok=True)
-            folder_paths.add_model_folder_path("loras", new_lora_path)
-            lora_path = new_lora_path / lora_name
-            if not os.path.exists(lora_path):
-                os.makedirs(Path(lora_path).parent)
+            if os.path.exists(new_lora_dir):
+                shutil.rmtree(new_lora_dir, ignore_errors=False, onerror=None)
             
-            if "drive.google" in lora_name:
-                lora_path = download_file_from_url(url=lora_name, download_path=lora_path)
+            os.makedirs(new_lora_dir, exist_ok=True)
+            folder_paths.add_model_folder_path("loras", new_lora_dir)
+            
+            local_lora_name = self.get_local_lora_name(lora_name)
+        
+            local_lora_path = new_lora_dir / local_lora_name
+            if not os.path.exists(local_lora_path):
+                os.makedirs(Path(local_lora_path).parent)
+            
+                
+            if "drive.google" in lora_url_or_path:
+                local_lora_path = download_file_from_url(url=lora_url_or_path, download_path=local_lora_path)
             else:
-                lora_path = download_file_from_s3_bucket(bucket_file_path=lora_name, download_path=lora_path)
+                local_lora_path = download_file_from_s3_bucket(bucket_file_path=lora_url_or_path, download_path=local_lora_path)
 
         if self.loaded_lora is not None:
-            if self.loaded_lora[0] == lora_path:
+            if self.loaded_lora[0] == local_lora_path:
                 lora = self.loaded_lora[1]
             else:
                 self.loaded_lora = None
         
-        lora_path = str(lora_path)
-        print(f"Downloaded LoRA path: {lora_path}")
+        local_lora_path = str(local_lora_path)
+        print(f"Downloaded LoRA path: {local_lora_path}")
         
         if lora is None:
-            if lora_path and "checkpoint" in lora_path:
+            if local_lora_path and "checkpoint" in local_lora_path:
                 
-                lora = self.load_checkpoint_lora(model, clip, lora_path)
-                self.loaded_lora = (lora_path, lora)
+                lora = self.load_checkpoint_lora(model, clip, local_lora_path)
+                self.loaded_lora = (local_lora_path, lora)
             else:
-                lora = comfy.utils.load_torch_file(lora_path, safe_load=True)
-                self.loaded_lora = (lora_path, lora)
+                lora = comfy.utils.load_torch_file(local_lora_path, safe_load=True)
+                self.loaded_lora = (local_lora_path, lora)
 
         model_lora, clip_lora = comfy.sd.load_lora_for_models(
             model, clip, lora, strength_model, strength_clip)
